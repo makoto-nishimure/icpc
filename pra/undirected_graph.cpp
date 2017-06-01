92d2fc96541c838c16083938bc188610feecf48b
n231066m@mail.kyutech.jp <makoto@makoto.local>
2017-06-01 19:56:13 +0900

    i cant

--- pra/undirected_graph.cpp
+++ pra/undirected_graph.cpp
@@ -4,10 +4,12 @@
 #include <iostream>
 #include <stdlib.h>
 #include <bitset>
+#include <stack>
 
 #define MAX 800
 
 std::bitset<MAX> flag;  //admのnodes通過フラグ
+std::stack<int> root;
 
 //depth first search
 //一番遠いノードの番号を返す
@@ -28,7 +30,53 @@ void dfs(int n, int sn, int **adm, int *depth, int cd) {
   
 }
 
-int main(void) {
+bool memo_root(int n, int en, int eten, int **adm) {
+
+  // std::cout << "current node is " << en << std::endl;
+  
+  bool rf;//root flag
+  rf = false;
+  if(en == eten){
+    rf = true;
+    return rf;
+  }
+    
+  flag.set(en);
+  for(int i = 0; i <= n; i++) {
+    if(adm[en][i]) {
+	if(!flag[i]){
+	  if(memo_root(n, i, eten, adm)) {
+	    root.push(i);
+	    rf = true;
+	    break;
+	  }
+	}
+      }
+  }
+  return rf;
+}
+
+int cal_cost(int n, int *d) {
+
+  int sum, size, tmp;
+
+  sum = 0;
+  size = root.size();
+  for(int i = 0; i < size; i++) {
+    tmp = root.top();
+    root.pop();
+    //std::cout << tmp << std::endl;
+    if(tmp == 1)
+      sum += 0;
+    else
+      sum += d[tmp-2];
+  }
+  return sum;
+
+}
+
+
+int main(int argc, char *argv[]) {
 
   int i,j,k;
   //int adm[MAX][MAX]; //adjacency matrix 隣接行列
@@ -50,15 +98,31 @@ int main(void) {
   //start inputs
   int n;  //number of nodes
   int sn; //start node
+  std::stack<int> len; //list of en
   int en; //end node
+  std::stack<int> leten; //list of eten
   int eten; //end to end node
-  int tmp;
+  int tmp, temp;
+  int sum; //root cost sum
+  int size, sz; 
+  int fn, nn; //first and next node
+  int cc, wr;  //current cost ,worst root
 
-  std::stack<int> root;
+
+  while(true){
+
+    n = sn = en = eten = tmp =temp = sum =size =sz= fn= nn =cc =wr =0;
+    
+    for(i = 0; i < MAX; i++) {
+      flag.reset(i);
+      dic.reset(i);
+    }
 
 
-  
+    
   std::cin >> n;
+  if(n == 0)
+    break;
   for(i = 0; i < n-1; i++)
     std::cin >> p[i];
   for(i = 0; i < n-1; i++)
@@ -119,12 +183,19 @@ int main(void) {
       en = i;
     }
   }
+  
+  for(i = 0; i <= n; i++) {
+    if(tmp == depth[i])
+      len.push(i);
+  }
+  
   //end dfs(first time)
   for(i = 0; i < MAX; i++)
     flag.reset(i);
   //for(i = 0; i < n; i++)
   //std::cout << depth[i];
-  std::cout << en << std::endl;
+  
+  //std::cout << "end node: " << en << std::endl;
 
   //depth first search (second time)
   
@@ -135,21 +206,98 @@ int main(void) {
 
   tmp = 0;
   for(i = 0; i <= n; i++){
+    //std::cout << "i : " << i << ", depth: " << depth[i] << std::endl;
     if(tmp < depth[i]) {
       tmp = depth[i];
-      eten = i;
     }
   }
   //end dfs(second time)
 
-  std::cout << eten << std::endl;
-
-  //root of from end node to end to end node
-
-
+  //list up eten
+  for(i = 0; i <= n; i++){
+    if(tmp == depth[i])
+      leten.push(i);
+  }  
+ 
 
   
-  return 0;
+  //dicide most heavy root
+  size = len.size();
+  sz = leten.size();
+  //std::cout << size << " " << sz << std::endl;
 
+  if(sz > 0) {
+    int *array;
+    array = (int *)malloc(sizeof(int) * sz);
+    for(k = 0; k < sz; k++) {
+      array[k] = leten.top();
+      leten.pop();
+    }
+    
+    //std::cout << "size: " << size << std::endl;
+    wr = 0;
+    for(i = 0; i < size; i++) {
+      
+      temp = len.top();
+      len.pop();
+      
+      
+      for(k = 0; k < sz; k++) {
+	tmp = array[k];
+	if(temp == tmp)
+	  break;
+	
+	//root of from end node to elements of leten
+	for(j = 0; j < MAX; j++)
+	  flag.reset(j);
+	
+	memo_root(n, temp, tmp, adm);
+	root.push(temp);
+	cc = cal_cost(n, d);
+	// std::cout << tmp << " " << cc << std::endl;
+	if(wr < cc) {
+	  wr = cc;
+	  en = temp;
+	  eten = tmp;
+	}
+      }
+    }
+    free(array);
+
+    
+    //std::cout << "wr: " << wr << std::endl;
+    //std::cout << "eten: " << eten << std::endl;
+    
+    tmp = 0;
+    sum = 0;
+    std::cout << "wr: " << wr << std::endl;
+    for(j = 0; j < n-1; j++)
+    std::cout << dic[j];
+    std::cout << std::endl;
+    for(k = 1; k < n; k++)
+      if(dic[k] && ((k-2) >= 0))
+	tmp+=d[k-2];
+    
+    tmp*=2;
+    tmp-=wr;
+    
+    for(j = 0; j < n-1; j++)
+      sum+=d[j];
+    
+    std::cout << "sum: " << sum << std::endl;
+    std::cout << "tmp: " << tmp << std::endl;
+    sum+=tmp;
+    std::cout << sum << std::endl;
+  }
+  else {
+    for(j = 0; j < n-1; j++)
+      sum+=d[j];
+    std::cout << sum << std::endl;
+    
+  }
+  
+  }
+  return 0;
+  
   
 }
