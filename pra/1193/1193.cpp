d85d1c79ce9456f52909b5c7cf640baf35a3e4e5
n231066m@mail.kyutech.jp <makoto@dhcp202.ipl-e.ai.kyutech.ac.jp>
2017-07-03 20:10:57 +0900

    0703

--- pra/1193/1193.cpp
+++ pra/1193/1193.cpp
@@ -17,17 +17,21 @@ int main()
       cin >> array[i][j];
 
   int num;
-  for (size_t t = 0; t < 5; t++) {
-  //while (true) {
+  //for (size_t t = 0; t < 5; t++) {
+  while (true) {
     for (size_t i = 0; i < h; i++) {
-      int tmp;// array[i][0];
+      int tmp =  array[i][0];
       int count = 0;
+      int start;
       num = 0;
       for (size_t j = 0; j < 5; j++) {
 	if (tmp == array[i][j]) {
+	  if (!count) 
+	    start = j;
 	  count++;
 	  num = tmp;
 	} else if (count < 3) {
+	  start = j;
 	  tmp = array[i][j];
 	  count = 1;
 	  num = 0;
@@ -35,7 +39,7 @@ int main()
 	  break;
       }
       if (count > 2 && num > 0) {
-	for (size_t j = 0; j < 5; j++)
+	for (int j = start; j < start + count; j++)
 	  if(num == array[i][j])
 	    array[i][j] = 0;
 	//cout << "num::" << num << endl;
@@ -58,7 +62,8 @@ int main()
       cout << endl;
     } cout << endl;
     */
-    
+
+    /*
   for (int k = h-1; k > 0; k--) {
     for (size_t i = 0; i < 5; ) {
       if (array[k][i] == 0) {
@@ -70,7 +75,7 @@ int main()
 	if (nzc != 0) {
 	  for (int j = k; j > 0; j--) {
 	    array[j][i] = array[j-1][i];	  
-	}
+	  }
 	  array[0][i] = 0;
 	}
 	else
@@ -80,17 +85,31 @@ int main()
 	i++;
     }
   }
-  
+    */
+
+    for (size_t i = 0; i < 10; i++)
+    for (size_t k = 0; k < h-1; k++) {
+      for (size_t j = 0; j < 5; j++) {
+	if (array[k][j] > 0 && array[k+1][j] == 0) {
+	  array[k+1][j] = array[k][j];
+	  array[k][j] = 0;
+	}
+      }
+    }
+
    for (size_t i = 0; i < h; i++) 
     for (size_t j = 0; j < 5; j++)
       if (array[i][j] == 0)
 	array[i][j] = -1;
    
   
-   /* 
+   /*
   for (size_t i = 0; i < h; i++) {
     for (size_t j = 0; j < 5; j++)
-      cout << array[i][j] << " ";
+      if (array[i][j] == -1)
+	cout << "  ";
+      else 
+	cout << array[i][j] << " ";
     cout << endl;
   } cout << endl;
    */
---
+++ pra/1193/aa.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(void) {
+
+  int h;
+  cin >> h;
+
+  int array[h][5];
+  int sco = 0;
+
+  for (size_t i = 0; i < h; i++)
+    for (size_t j = 0; j < 5; j++)
+      cin >> array[i][j];
+  
+  for (size_t i = 0; i < h; i++) {
+    int tmp = array[i][0];
+    int count = 0;
+    int start;
+    
+    for (size_t j = 0; j < 5; j++) {
+
+
+    }
+  }
+  
+}
